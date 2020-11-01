90be66e080aa7a7340f3eb4de1e799b22c85bd3b
ealmario <ee.almario@gmail.com>
2020-11-01 13:59:52 +0800

    npm install styled-components | Add reset in index.css | Use styled-components in App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
 import Board from "./Board";
-import "./App.css";
+import styled from "styled-components";
 
 /** Simple app that just shows the LightsOut game. */
+const AppDiv = styled.div`
+  background-color: #003;
+  color: #fff;
+  font-size: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <AppDiv>
         <Board />
-      </div>
+      </AppDiv>
     );
   }
 }
