2be1965e3a0a015639fa34c323d7ee93882d72f3
ealmario <ee.almario@gmail.com>
2020-11-28 18:27:25 +0800

    Updates

---
+++ precache-manifest.f780710426539bef6deb4e932db5ae35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ff4acf5c7b267649b17",
+    "url": "/lights-out/static/css/main.d8705ff3.chunk.css"
+  },
+  {
+    "revision": "7ff4acf5c7b267649b17",
+    "url": "/lights-out/static/js/main.7ff4acf5.chunk.js"
+  },
+  {
+    "revision": "f73266bf33c116f8d3ff",
+    "url": "/lights-out/static/js/1.f73266bf.chunk.js"
+  },
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "4f643081ef5d6dc84f6ff2197309a13d",
+    "url": "/lights-out/index.html"
+  }
+];
